04ab9f0f36c1fb2320454d561a0030a0558feebb
Oleksy Lykov <lukov_jr@ukr.net>
2023-07-24 16:03:23 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,18 +2,20 @@ import express from "express";
 import contactControllers from "../../controllers/contact-controller.js";
 import contactsSchemas from "../../schemas/contacts-schemas.js";
 
-import {validateBody} from "../../decorators/index.js";
+import {validateBody, isValidId} from "../../decorators/index.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get('/', contactControllers.getAll)
 
-contactsRouter.get('/:id', contactControllers.getById)
+contactsRouter.get('/:id', isValidId, contactControllers.getById)
 
 contactsRouter.post('/', validateBody(contactsSchemas.contactsAddSchema), contactControllers.add)
 
-contactsRouter.delete('/:id', contactControllers.deleteById)
+contactsRouter.delete('/:id', isValidId, contactControllers.deleteById)
 
-contactsRouter.put('/:id', validateBody(contactsSchemas.contactsAddSchema), contactControllers.updateById)
+contactsRouter.put('/:id', isValidId, validateBody(contactsSchemas.contactsAddSchema), contactControllers.updateById)
+
+contactsRouter.patch('/:id/favorite', isValidId, validateBody(contactsSchemas.updateFavoriteSchema), contactControllers.updateStatusContact)
 
 export default contactsRouter;
