b7c9fa2e5766615540e2b8f41a7c9d912746030e
Oleksy Lykov <lukov_jr@ukr.net>
2023-08-15 22:56:03 +0300

    fix: виправив помилку

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -43,7 +43,7 @@ const verify = async (req, res) => {
     if (!user) {
         throw HttpError(404, "User not found");
     }
-    await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: " " });
+    await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: null });
 
     res.json({
         message: "Verification successful",
